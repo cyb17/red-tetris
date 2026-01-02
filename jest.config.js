0030c8b8d9cfc9383337b282c83ff62f62d6e3f2
cyb17 <yabingc7@gmail.com>
2026-01-02 15:47:09 +0100

    modify project infrastructure

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  collectCoverageFrom: ['**/*.js', '!node_modules/**', '!coverage/**', '!jest.config.js'],
-  testMatch: ['**/*.test.js'],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-};
